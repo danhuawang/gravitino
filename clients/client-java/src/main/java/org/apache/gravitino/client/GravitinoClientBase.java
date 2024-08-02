danhuawang/gravitino

commit edd5961f435ab3a79614f38ee762400dbbe9756b

[#3084] feat(client-java): Add @Deprecated annotations and explanations across GravitinoClientBase   (#4338)
I added @Deprecated annotations to relevant methods in the
GravitinoClientBase class to improve code clarity and guide developers
towards using updated methods. - Deprecated `getVersion()` method in
favor of `serverVersion()`. - Provided explanations within the
annotations to clarify why the deprecation was applied and what should
be used instead. This change ensures that outdated methods are clearly
marked and helps maintain codebase consistency. Closes #3084

As part of an effort to keep the Gravitino Java client codebase clean
and up-to-date, deprecated annotations were added to certain methods in
the GravitinoClientBase class. The `getVersion()` method was marked as
deprecated since it duplicates the functionality provided by the
`serverVersion()` method. An inline comment was provided to guide
developers towards using the recommended method.

This is my first contribution to the project, and I look forward to
contributing further. Please review the changes and provide feedback if
needed.

### What changes were proposed in this pull request?

This pull request adds @Deprecated annotations to specific methods
within the GravitinoClientBase class. The getVersion() method was
deprecated in favor of serverVersion() to reduce redundancy and steer
developers towards the preferred method. Each deprecated method includes
a detailed comment explaining why the deprecation was applied and
suggesting the appropriate alternative.

### Why are the changes needed?

These changes are necessary to maintain a clean and up-to-date codebase.
By marking certain methods as deprecated, we help developers avoid using
outdated or redundant methods and encourage best practices within the
Gravitino Java client. Specifically:

The getVersion() method was deprecated as it duplicates the
functionality of serverVersion().

clients/client-java/src/main/java/org/apache/gravitino/client/GravitinoClientBase.java
@@ -152,7 +152,8 @@ public abstract class GravitinoClientBase implements Closeable {
   /**
    * Retrieves the version of the Gravitino API.
    *
-   * @return A GravitinoVersion instance representing the version of the Gravitino API.
+   * @deprecated This method is deprecated because it is a duplicate of {@link #serverVersion()}.
+   *             Use {@link #serverVersion()} instead for clarity and consistency.
    */
   @Deprecated
   @InlineMe(replacement = "this.serverVersion()")

commit 097c1e2433541e1ff4edf67f55edda19c981cf8f

[HOTFIX]  fix spotless check error (#4340)
### What changes were proposed in this pull request?
fix splot check error

### Why are the changes needed?
ci failed for spotless check, introduced by #4338 
```
Execution failed for task ':clients:client-java:spotlessJavaCheck'.
> The following files had format violations:
      src/main/java/org/apache/gravitino/client/GravitinoClientBase.java
          @@ -153,7 +153,7 @@
           ···*·Retrieves·the·version·of·the·Gravitino·API.
           ···*
           ···*·@deprecated·This·method·is·deprecated·because·it·is·a·duplicate·of·{@link·#serverVersion()}.
          -···*·············Use·{@link·#serverVersion()}·instead·for·clarity·and·consistency.
          +···*·····Use·{@link·#serverVersion()}·instead·for·clarity·and·consistency.
           ···*/
           ··@Deprecated
           ··@InlineMe(replacement·=·"this.serverVersion()")
  Run './gradlew :clients:client-java:spotlessApply' to fix these violations.
```

### Does this PR introduce _any_ user-facing change?
no

### How was this patch tested?
existing tests

clients/client-java/src/main/java/org/apache/gravitino/client/GravitinoClientBase.java
@@ -152,8 +152,9 @@ public abstract class GravitinoClientBase implements Closeable {
   /**
    * Retrieves the version of the Gravitino API.
    *
+   * @return A GravitinoVersion instance representing the version of the Gravitino API.
    * @deprecated This method is deprecated because it is a duplicate of {@link #serverVersion()}.
-   *             Use {@link #serverVersion()} instead for clarity and consistency.
+   *     Use {@link #serverVersion()} instead for clarity and consistency.
    */
   @Deprecated
   @InlineMe(replacement = "this.serverVersion()")