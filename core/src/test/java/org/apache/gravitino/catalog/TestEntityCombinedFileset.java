danhuawang/gravitino

commit dcff26d551e3d315324e22a6e468ae0531de3d94

[#6892] feat(core): support fileset multiple locations in core module (#6922)
### What changes were proposed in this pull request?

support fileset multiple locations in the core and catalog module

### Why are the changes needed?

Fix: #6892

### Does this PR introduce _any_ user-facing change?

no

### How was this patch tested?

tests added

core/src/main/java/org/apache/gravitino/catalog/EntityCombinedFileset.java
@@ -40,14 +40,6 @@ public final class EntityCombinedFileset implements Fileset {
     this.filesetEntity = filesetEntity;
   }
 
-  public FilesetEntity filesetEntity() {
-    return filesetEntity;
-  }
-
-  public Fileset fileset() {
-    return fileset;
-  }
-
   public static EntityCombinedFileset of(Fileset fileset, FilesetEntity filesetEntity) {
     return new EntityCombinedFileset(fileset, filesetEntity);
   }
@@ -77,8 +69,8 @@ public final class EntityCombinedFileset implements Fileset {
   }
 
   @Override
-  public String storageLocation() {
-    return fileset.storageLocation();
+  public Map<String, String> storageLocations() {
+    return fileset.storageLocations();
   }
 
   @Override

catalogs/catalog-hadoop/src/main/java/org/apache/gravitino/catalog/hadoop/HadoopFileset.java
@@ -31,7 +31,7 @@ public class HadoopFileset extends BaseFileset {
       HadoopFileset fileset = new HadoopFileset();
       fileset.name = name;
       fileset.comment = comment;
-      fileset.storageLocation = storageLocation;
+      fileset.storageLocations = storageLocations;
       fileset.type = type;
       fileset.properties = properties;
       fileset.auditInfo = auditInfo;

catalogs/catalog-hadoop/src/main/java/org/apache/gravitino/catalog/hadoop/HadoopFilesetPropertiesMetadata.java
@@ -19,6 +19,7 @@
 package org.apache.gravitino.catalog.hadoop;
 
 import static org.apache.gravitino.file.Fileset.PROPERTY_CATALOG_PLACEHOLDER;
+import static org.apache.gravitino.file.Fileset.PROPERTY_DEFAULT_LOCATION_NAME;
 import static org.apache.gravitino.file.Fileset.PROPERTY_FILESET_PLACEHOLDER;
 import static org.apache.gravitino.file.Fileset.PROPERTY_LOCATION_PLACEHOLDER_PREFIX;
 import static org.apache.gravitino.file.Fileset.PROPERTY_SCHEMA_PLACEHOLDER;
@@ -64,6 +65,14 @@ public class HadoopFilesetPropertiesMetadata extends BasePropertiesMetadata {
                 null /* default value */,
                 false /* hidden */,
                 false /* reserved */))
+        .put(
+            PROPERTY_DEFAULT_LOCATION_NAME,
+            PropertyEntry.stringOptionalPropertyEntry(
+                PROPERTY_DEFAULT_LOCATION_NAME,
+                "The default location name for the fileset",
+                true /* immutable */,
+                null,
+                false /* hidden */))
         .putAll(KerberosConfig.KERBEROS_PROPERTY_ENTRIES)
         .putAll(AuthenticationConfig.AUTHENTICATION_PROPERTY_ENTRIES)
         .putAll(CredentialConfig.CREDENTIAL_PROPERTY_ENTRIES);

catalogs/catalog-hadoop/src/main/java/org/apache/gravitino/catalog/hadoop/SecureHadoopCatalogOperations.java
@@ -50,6 +50,7 @@ import org.apache.gravitino.exceptions.FilesetAlreadyExistsException;
 import org.apache.gravitino.exceptions.NoSuchCatalogException;
 import org.apache.gravitino.exceptions.NoSuchEntityException;
 import org.apache.gravitino.exceptions.NoSuchFilesetException;
+import org.apache.gravitino.exceptions.NoSuchLocationNameException;
 import org.apache.gravitino.exceptions.NoSuchSchemaException;
 import org.apache.gravitino.exceptions.NonEmptySchemaException;
 import org.apache.gravitino.exceptions.SchemaAlreadyExistsException;
@@ -105,11 +106,11 @@ public class SecureHadoopCatalogOperations
   }
 
   @Override
-  public Fileset createFileset(
+  public Fileset createMultipleLocationFileset(
       NameIdentifier ident,
       String comment,
       Fileset.Type type,
-      String storageLocation,
+      Map<String, String> storageLocations,
       Map<String, String> properties)
       throws NoSuchSchemaException, FilesetAlreadyExistsException {
     String apiUser = PrincipalUtils.getCurrentUserName();
@@ -120,8 +121,8 @@ public class SecureHadoopCatalogOperations
     return userContext.doAs(
         () -> {
           setUser(apiUser);
-          return hadoopCatalogOperations.createFileset(
-              ident, comment, type, storageLocation, properties);
+          return hadoopCatalogOperations.createMultipleLocationFileset(
+              ident, comment, type, storageLocations, properties);
         },
         ident);
   }
@@ -233,9 +234,9 @@ public class SecureHadoopCatalogOperations
   }
 
   @Override
-  public String getFileLocation(NameIdentifier ident, String subPath)
-      throws NoSuchFilesetException {
-    return hadoopCatalogOperations.getFileLocation(ident, subPath);
+  public String getFileLocation(NameIdentifier ident, String subPath, String locationName)
+      throws NoSuchFilesetException, NoSuchLocationNameException {
+    return hadoopCatalogOperations.getFileLocation(ident, subPath, locationName);
   }
 
   @Override
@@ -260,7 +261,16 @@ public class SecureHadoopCatalogOperations
   @Override
   public List<PathContext> getPathContext(NameIdentifier filesetIdentifier) {
     Fileset fileset = loadFileset(filesetIdentifier);
-    String path = fileset.storageLocation();
+    Map<String, String> locations = fileset.storageLocations();
+    Preconditions.checkArgument(
+        locations != null && !locations.isEmpty(),
+        "No storage locations found for fileset: " + filesetIdentifier);
+
+    // todo: support multiple storage locations
+    Preconditions.checkArgument(
+        locations.size() == 1, "Only one storage location is supported for fileset now");
+
+    String path = locations.values().iterator().next();
     Preconditions.checkState(
         StringUtils.isNotBlank(path), "The location of fileset should not be empty.");
 

catalogs/catalog-hadoop/src/test/java/org/apache/gravitino/catalog/hadoop/integration/test/HadoopCatalogIT.java
@@ -18,6 +18,8 @@
  */
 package org.apache.gravitino.catalog.hadoop.integration.test;
 
+import static org.apache.gravitino.file.Fileset.LOCATION_NAME_UNKNOWN;
+import static org.apache.gravitino.file.Fileset.PROPERTY_DEFAULT_LOCATION_NAME;
 import static org.apache.gravitino.file.Fileset.PROPERTY_LOCATION_PLACEHOLDER_PREFIX;
 import static org.apache.gravitino.file.Fileset.Type.MANAGED;
 
@@ -202,8 +204,10 @@ public class HadoopCatalogIT extends BaseIT {
     Assertions.assertEquals("comment", fileset.comment());
     Assertions.assertEquals(MANAGED, fileset.type());
     Assertions.assertEquals(storageLocation, fileset.storageLocation());
-    Assertions.assertEquals(1, fileset.properties().size());
+    Assertions.assertEquals(2, fileset.properties().size());
     Assertions.assertEquals("v1", fileset.properties().get("k1"));
+    Assertions.assertEquals(
+        LOCATION_NAME_UNKNOWN, fileset.properties().get(PROPERTY_DEFAULT_LOCATION_NAME));
 
     // test create a fileset that already exist
     Assertions.assertThrows(
@@ -224,7 +228,9 @@ public class HadoopCatalogIT extends BaseIT {
         storageLocation(filesetName2),
         fileset2.storageLocation(),
         "storage location should be created");
-    Assertions.assertEquals(ImmutableMap.of(), fileset2.properties(), "properties should be empty");
+    Assertions.assertEquals(
+        ImmutableMap.of(PROPERTY_DEFAULT_LOCATION_NAME, LOCATION_NAME_UNKNOWN),
+        fileset2.properties());
 
     // create fileset with placeholder in storage location
     String filesetName4 = "test_create_fileset_with_placeholder";
@@ -239,7 +245,9 @@ public class HadoopCatalogIT extends BaseIT {
     Assertions.assertEquals("comment", fileset4.comment());
     Assertions.assertEquals(MANAGED, fileset4.type());
     Assertions.assertEquals(expectedStorageLocation4, fileset4.storageLocation());
-    Assertions.assertEquals(0, fileset4.properties().size(), "properties should be empty");
+    Assertions.assertEquals(1, fileset4.properties().size(), "properties should be empty");
+    Assertions.assertEquals(
+        LOCATION_NAME_UNKNOWN, fileset4.properties().get(PROPERTY_DEFAULT_LOCATION_NAME));
 
     // create fileset with null fileset name
     Assertions.assertThrows(
@@ -303,10 +311,12 @@ public class HadoopCatalogIT extends BaseIT {
     Assertions.assertEquals("这是中文comment", fileset.comment());
     Assertions.assertEquals(MANAGED, fileset.type());
     Assertions.assertEquals(storageLocation, fileset.storageLocation());
-    Assertions.assertEquals(3, fileset.properties().size());
+    Assertions.assertEquals(4, fileset.properties().size());
     Assertions.assertEquals("v1", fileset.properties().get("k1"));
     Assertions.assertEquals("中文测试test", fileset.properties().get("test"));
     Assertions.assertEquals("test1", fileset.properties().get("中文key"));
+    Assertions.assertEquals(
+        LOCATION_NAME_UNKNOWN, fileset.properties().get(PROPERTY_DEFAULT_LOCATION_NAME));
   }
 
   @Test
@@ -328,10 +338,12 @@ public class HadoopCatalogIT extends BaseIT {
     Assertions.assertEquals("comment", fileset.comment());
     Assertions.assertEquals(Fileset.Type.EXTERNAL, fileset.type());
     Assertions.assertEquals(storageLocation, fileset.storageLocation());
-    Assertions.assertEquals(1, fileset.properties().size());
+    Assertions.assertEquals(2, fileset.properties().size());
     Assertions.assertEquals("v1", fileset.properties().get("k1"));
     Assertions.assertTrue(
         fileSystem.exists(new Path(storageLocation)), "storage location should be created");
+    Assertions.assertEquals(
+        LOCATION_NAME_UNKNOWN, fileset.properties().get(PROPERTY_DEFAULT_LOCATION_NAME));
 
     // create fileset with storage location that not exist
     String filesetName2 = "test_external_fileset_no_exist";
@@ -538,9 +550,11 @@ public class HadoopCatalogIT extends BaseIT {
     Assertions.assertEquals(MANAGED, newFileset.type(), "type should not be change");
     Assertions.assertEquals(
         storageLocation, newFileset.storageLocation(), "storage location should not be change");
-    Assertions.assertEquals(1, newFileset.properties().size(), "properties should not be change");
+    Assertions.assertEquals(2, newFileset.properties().size(), "properties should not be change");
     Assertions.assertEquals(
         "v1", newFileset.properties().get("k1"), "properties should not be change");
+    Assertions.assertEquals(
+        LOCATION_NAME_UNKNOWN, newFileset.properties().get(PROPERTY_DEFAULT_LOCATION_NAME));
   }
 
   @Test
@@ -568,9 +582,11 @@ public class HadoopCatalogIT extends BaseIT {
     Assertions.assertEquals(MANAGED, newFileset.type(), "type should not be change");
     Assertions.assertEquals(
         storageLocation, newFileset.storageLocation(), "storage location should not be change");
-    Assertions.assertEquals(1, newFileset.properties().size(), "properties should not be change");
+    Assertions.assertEquals(2, newFileset.properties().size(), "properties should not be change");
     Assertions.assertEquals(
         "v1", newFileset.properties().get("k1"), "properties should not be change");
+    Assertions.assertEquals(
+        LOCATION_NAME_UNKNOWN, newFileset.properties().get(PROPERTY_DEFAULT_LOCATION_NAME));
   }
 
   @Test
@@ -596,9 +612,11 @@ public class HadoopCatalogIT extends BaseIT {
     Assertions.assertEquals(MANAGED, newFileset.type(), "type should not be change");
     Assertions.assertEquals(
         storageLocation, newFileset.storageLocation(), "storage location should not be change");
-    Assertions.assertEquals(1, newFileset.properties().size(), "properties should not be change");
+    Assertions.assertEquals(2, newFileset.properties().size(), "properties should not be change");
     Assertions.assertEquals(
         "v2", newFileset.properties().get("k1"), "properties should be updated");
+    Assertions.assertEquals(
+        LOCATION_NAME_UNKNOWN, newFileset.properties().get(PROPERTY_DEFAULT_LOCATION_NAME));
   }
 
   @Test
@@ -624,7 +642,9 @@ public class HadoopCatalogIT extends BaseIT {
     Assertions.assertEquals(MANAGED, newFileset.type(), "type should not be change");
     Assertions.assertEquals(
         storageLocation, newFileset.storageLocation(), "storage location should not be change");
-    Assertions.assertEquals(0, newFileset.properties().size(), "properties should be removed");
+    Assertions.assertEquals(1, newFileset.properties().size(), "properties should be removed");
+    Assertions.assertEquals(
+        LOCATION_NAME_UNKNOWN, newFileset.properties().get(PROPERTY_DEFAULT_LOCATION_NAME));
   }
 
   @Test
@@ -650,9 +670,11 @@ public class HadoopCatalogIT extends BaseIT {
     Assertions.assertEquals(MANAGED, newFileset.type(), "type should not be changed");
     Assertions.assertEquals(
         storageLocation, newFileset.storageLocation(), "storage location should not be changed");
-    Assertions.assertEquals(1, newFileset.properties().size(), "properties should not be changed");
+    Assertions.assertEquals(2, newFileset.properties().size(), "properties should not be changed");
     Assertions.assertEquals(
         "v1", newFileset.properties().get("k1"), "properties should not be changed");
+    Assertions.assertEquals(
+        LOCATION_NAME_UNKNOWN, newFileset.properties().get(PROPERTY_DEFAULT_LOCATION_NAME));
   }
 
   @Test

catalogs/catalog-hadoop/src/test/java/org/apache/gravitino/catalog/hadoop/integration/test/HadoopUserImpersonationIT.java
@@ -367,7 +367,10 @@ public class HadoopUserImpersonationIT extends BaseIT {
     Assertions.assertEquals("comment", fileset.comment());
     Assertions.assertEquals(Fileset.Type.MANAGED, fileset.type());
     Assertions.assertEquals(storageLocation, fileset.storageLocation());
-    Assertions.assertEquals(1, fileset.properties().size());
+    Assertions.assertEquals(2, fileset.properties().size());
+    Assertions.assertTrue(
+        fileset.properties().containsKey(Fileset.PROPERTY_DEFAULT_LOCATION_NAME),
+        "properties should contain default location name");
     Assertions.assertEquals("v1", fileset.properties().get("k1"));
 
     // test create a fileset that already exist
@@ -394,7 +397,9 @@ public class HadoopUserImpersonationIT extends BaseIT {
         storageLocation(filesetName2),
         fileset2.storageLocation(),
         "storage location should be created");
-    Assertions.assertEquals(ImmutableMap.of(), fileset2.properties(), "properties should be empty");
+    Assertions.assertTrue(
+        fileset2.properties().containsKey(Fileset.PROPERTY_DEFAULT_LOCATION_NAME),
+        "properties should contain default location name");
 
     // create fileset with null fileset name
     Assertions.assertThrows(

commit 38f6c95caf2c95148e6dc07938e6075cf6461b4c

[MINOR] Revert changes that remove getter method in EntityCombinedFileset (#6975)
### What changes were proposed in this pull request?

Add back getter method in EntityCombinedFileset that is caused by #6922

### Why are the changes needed?

It's used by other import project.

### Does this PR introduce _any_ user-facing change?

N/A

### How was this patch tested?

N/A.

core/src/main/java/org/apache/gravitino/catalog/EntityCombinedFileset.java
@@ -40,6 +40,14 @@ public final class EntityCombinedFileset implements Fileset {
     this.filesetEntity = filesetEntity;
   }
 
+  public FilesetEntity filesetEntity() {
+    return filesetEntity;
+  }
+
+  public Fileset fileset() {
+    return fileset;
+  }
+
   public static EntityCombinedFileset of(Fileset fileset, FilesetEntity filesetEntity) {
     return new EntityCombinedFileset(fileset, filesetEntity);
   }

core/src/test/java/org/apache/gravitino/catalog/TestEntityCombinedFileset.java
@@ -1,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.gravitino.catalog;
+
+import static org.apache.gravitino.file.Fileset.LOCATION_NAME_UNKNOWN;
+
+import com.google.common.collect.ImmutableMap;
+import org.apache.gravitino.file.Fileset;
+import org.apache.gravitino.meta.AuditInfo;
+import org.apache.gravitino.meta.FilesetEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class TestEntityCombinedFileset {
+
+  /**
+   * This test has no real assertions, but it's just to use the getter methods, by calling them
+   * others will not delete the getters by mistake.
+   */
+  @Test
+  void testGetters() {
+    Fileset fileset = Mockito.mock(Fileset.class);
+    FilesetEntity filesetEntity =
+        FilesetEntity.builder()
+            .withId(1L)
+            .withName("fileset")
+            .withAuditInfo(AuditInfo.EMPTY)
+            .withFilesetType(Fileset.Type.MANAGED)
+            .withStorageLocations(ImmutableMap.of(LOCATION_NAME_UNKNOWN, "testLocation"))
+            .withProperties(ImmutableMap.of())
+            .build();
+
+    EntityCombinedFileset entityCombinedFileset = EntityCombinedFileset.of(fileset, filesetEntity);
+    Assertions.assertEquals(fileset, entityCombinedFileset.fileset());
+    Assertions.assertEquals(filesetEntity, entityCombinedFileset.filesetEntity());
+  }
+}