fadbd19da670f675572dbe89f095f40002f5a275
Danhua Wang <danhua@datastrato.com>
2025-09-09 15:26:11 +0800

    [#427] Add jars for main (#429)
    ### What changes were proposed in this pull request?
    
    Put missing cloud jars into the Iceberg REST server libs and Iceberg
    catalog
    
    The branch-0.9 using Iceberg 1.6.1 ,while branch-1.0 using Iceberg 1.9.2
    ,so this pr target branch is main
    
    ### Why are the changes needed?
    
    Fix: #427 
    
    ### Does this PR introduce _any_ user-facing change?
    
    N/A
    
    ### How was this patch tested?
    
    Test locally

--- dev/docker/build-docker.sh
+++ dev/docker/build-docker.sh
@@ -87,7 +87,7 @@ fi
 
 if [ "${component_type}" == "datastrato-gravitino" ]; then
   . ${script_dir}/datastrato-gravitino/datastrato-gravitino-dependency.sh
-  build_args="--build-arg MYSQL_JDBC_DRIVER_NAME=${MYSQL_JDBC_DRIVER_NAME} --build-arg POSTGRESQL_JDBC_DRIVER_NAME=${POSTGRESQL_JDBC_DRIVER_NAME}"
+  build_args="--build-arg MYSQL_JDBC_DRIVER_NAME=${MYSQL_JDBC_DRIVER_NAME} --build-arg POSTGRESQL_JDBC_DRIVER_NAME=${POSTGRESQL_JDBC_DRIVER_NAME} --build-arg ICEBERG_AWS_BUNDLE_NAME=${ICEBERG_AWS_BUNDLE_NAME} --build-arg ICEBERG_GCP_BUNDLE_NAME=${ICEBERG_GCP_BUNDLE_NAME} --build-arg ICEBERG_AZURE_BUNDLE_NAME=${ICEBERG_AZURE_BUNDLE_NAME} --build-arg GCS_CONNECTOR_NAME=${GCS_CONNECTOR_NAME}"
 else
   echo "ERROR : ${component_type} is not a valid component type"
   usage
--- dev/docker/datastrato-gravitino/datastrato-gravitino-dependency.sh
+++ dev/docker/datastrato-gravitino/datastrato-gravitino-dependency.sh
@@ -21,6 +21,8 @@ set -ex
 gravitino_dir="$(dirname "${BASH_SOURCE-$0}")"
 gravitino_dir="$(cd "${gravitino_dir}">/dev/null; pwd)"
 gravitino_home="$(cd "${gravitino_dir}/../../..">/dev/null; pwd)"
+gravitino_iceberg_rest_dir="${gravitino_dir}/packages/gravitino/iceberg-rest-server/libs/"
+gravitino_iceberg_catalog_dir="${gravitino_dir}/packages/gravitino/catalogs/lakehouse-iceberg/libs"
 
 MYSQL_JDBC_DRIVER_VERSION=${MYSQL_VERSION:-"8.0.26"}
 MYSQL_JDBC_DRIVER_NAME="mysql-connector-java-${MYSQL_JDBC_DRIVER_VERSION}.jar"
@@ -29,6 +31,19 @@ MYSQL_JDBC_DIVER_DOWNLOAD_URL="https://repo1.maven.org/maven2/mysql/mysql-connec
 POSTGRESQL_JDBC_DRIVER_VERSION=${POSTGRESQL_VERSION:-"42.7.0"}
 POSTGRESQL_JDBC_DRIVER_NAME="postgresql-${POSTGRESQL_JDBC_DRIVER_VERSION}.jar"
 POSTGRESQL_JDBC_DRIVER_DOWNLOAD_URL="https://jdbc.postgresql.org/download/${POSTGRESQL_JDBC_DRIVER_NAME}"
+
+ICEBERG_VERSION=${ICEBERG_VERSION:-"1.9.2"}
+ICEBERG_AWS_BUNDLE_NAME="iceberg-aws-bundle-${ICEBERG_VERSION}.jar"
+ICEBERG_AWS_BUNDLE_DOWNLOAD_URL="https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-aws-bundle/${ICEBERG_VERSION}/${ICEBERG_AWS_BUNDLE_NAME}"
+ICEBERG_GCP_BUNDLE_NAME="iceberg-gcp-bundle-${ICEBERG_VERSION}.jar"
+ICEBERG_GCP_BUNDLE_DOWNLOAD_URL="https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-gcp-bundle/${ICEBERG_VERSION}/${ICEBERG_GCP_BUNDLE_NAME}"
+ICEBERG_AZURE_BUNDLE_NAME="iceberg-azure-bundle-${ICEBERG_VERSION}.jar"
+ICEBERG_AZURE_BUNDLE_DOWNLOAD_URL="https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-azure-bundle/${ICEBERG_VERSION}/${ICEBERG_AZURE_BUNDLE_NAME}"
+
+GCS_CONNECTOR_VERSION=${GCS_CONNECTOR_VERSION:-"hadoop2-2.2.18"}
+GCS_CONNECTOR_VERSION_SHORT=${GCS_CONNECTOR_VERSION_SHORT:-"2.2.18"}
+GCS_CONNECTOR_NAME="gcs-connector-${GCS_CONNECTOR_VERSION}-shaded.jar"
+GCS_CONNECTOR_DOWNLOAD_URL="https://github.com/GoogleCloudDataproc/hadoop-connectors/releases/download/v${GCS_CONNECTOR_VERSION_SHORT}/${GCS_CONNECTOR_NAME}"
 
 # make sure the submodule is initialized
 git -C "${gravitino_home}" submodule update --init --recursive
@@ -49,12 +64,40 @@ cp -r "${gravitino_home}/distribution/package" "${gravitino_dir}/packages/gravit
 # Copy the all file system bundles to the Hadoop catalog libs
 cp -r ${gravitino_home}/gravitino-oss/bundles/*-bundle/build/libs/*.jar "${gravitino_dir}/packages/gravitino/catalogs/fileset/libs"
 
+# Copy the all file system bundles to the Iceberg REST server libs
+find ${gravitino_home}/gravitino-oss/bundles/gcp/build/libs/ -name 'gravitino-gcp-*.jar' ! -name '*-empty.jar' -exec cp -v {} "${gravitino_iceberg_rest_dir}" \;
+find ${gravitino_home}/gravitino-oss/bundles/aws/build/libs/ -name 'gravitino-aws-*.jar' ! -name '*-empty.jar' -exec cp -v {} "${gravitino_iceberg_rest_dir}" \;
+find ${gravitino_home}/gravitino-oss/bundles/azure/build/libs/ -name 'gravitino-azure-*.jar' ! -name '*-empty.jar' -exec cp -v {} "${gravitino_iceberg_rest_dir}" \;
+find ${gravitino_home}/gravitino-oss/bundles/aliyun-bundle/build/libs/ -name 'gravitino-aliyun-bundle-*.jar' ! -name '*-empty.jar' -exec cp -v {} "${gravitino_iceberg_rest_dir}" \;
+
+# Copy the all file system bundles to the Iceberg catalog libs
+find ${gravitino_home}/gravitino-oss/bundles/gcp/build/libs/ -name 'gravitino-gcp-*.jar' ! -name '*-empty.jar' -exec cp -v {} "${gravitino_iceberg_catalog_dir}" \;
+find ${gravitino_home}/gravitino-oss/bundles/aws/build/libs/ -name 'gravitino-aws-*.jar' ! -name '*-empty.jar' -exec cp -v {} "${gravitino_iceberg_catalog_dir}" \;
+find ${gravitino_home}/gravitino-oss/bundles/azure/build/libs/ -name 'gravitino-azure-*.jar' ! -name '*-empty.jar' -exec cp -v {} "${gravitino_iceberg_catalog_dir}" \;
+find ${gravitino_home}/gravitino-oss/bundles/aliyun-bundle/build/libs/ -name 'gravitino-aliyun-bundle-*.jar' ! -name '*-empty.jar' -exec cp -v {} "${gravitino_iceberg_catalog_dir}" \;
+
 if [ ! -f "${gravitino_dir}/packages/${MYSQL_JDBC_DRIVER_NAME}" ]; then
   curl -L -s -o "${gravitino_dir}/packages/${MYSQL_JDBC_DRIVER_NAME}" "${MYSQL_JDBC_DIVER_DOWNLOAD_URL}"
 fi
 
 if [ ! -f "${gravitino_dir}/packages/${POSTGRESQL_JDBC_DRIVER_NAME}" ]; then
   curl -L -s -o "${gravitino_dir}/packages/${POSTGRESQL_JDBC_DRIVER_NAME}" "${POSTGRESQL_JDBC_DRIVER_DOWNLOAD_URL}"
+fi
+
+if [ ! -f "${gravitino_dir}/packages/${ICEBERG_AWS_BUNDLE_NAME}" ]; then
+  curl -L -s -o "${gravitino_dir}/packages/${ICEBERG_AWS_BUNDLE_NAME}" "${ICEBERG_AWS_BUNDLE_DOWNLOAD_URL}"
+fi
+
+if [ ! -f "${gravitino_dir}/packages/${ICEBERG_GCP_BUNDLE_NAME}" ]; then
+  curl -L -s -o "${gravitino_dir}/packages/${ICEBERG_GCP_BUNDLE_NAME}" "${ICEBERG_GCP_BUNDLE_DOWNLOAD_URL}"
+fi
+
+if [ ! -f "${gravitino_dir}/packages/${ICEBERG_AZURE_BUNDLE_NAME}" ]; then
+  curl -L -s -o "${gravitino_dir}/packages/${ICEBERG_AZURE_BUNDLE_NAME}" "${ICEBERG_AZURE_BUNDLE_DOWNLOAD_URL}"
+fi
+
+if [ ! -f "${gravitino_dir}/packages/${GCS_CONNECTOR_NAME}" ]; then
+  curl -L -s -o "${gravitino_dir}/packages/${GCS_CONNECTOR_NAME}" "${GCS_CONNECTOR_DOWNLOAD_URL}"
 fi
 
 # Keeping the container running at all times
